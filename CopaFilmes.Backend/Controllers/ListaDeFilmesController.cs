a1cfcaaaa72b3970dab26d05029156bbd8cf1620
Willson Ferreira <willson.ferreira@outlook.com>
2020-06-11 00:52:09 -0300

    Projeto Api

---
+++ CopaFilmes.Backend/Controllers/ListaDeFilmesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CopaFilmes.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class  Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sucesso";
+        }
+    }
+
+    
+}
